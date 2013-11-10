88f427223384e9bc7e4607ee8acbb1e8754561cd
Islam Abusida <izzy.az@gmail.com>
2013-11-09 21:26:53 -0700

    Initial Commit

---
+++ hadith/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hadith
+//
+//  Created by Islam Abusida on 11/9/13.
+//  Copyright (c) 2013 Islam Abusida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
